e685dffa3542617d373c72026c3d034502603711
chase-klaus <chase.klaus@gmail.com>
2021-01-27 22:09:26 -0500

    Queens & Rooks

--- scripts/queens.js
+++ scripts/queens.js
@@ -1,7 +1,49 @@
 'use strict';
 
 function calc (size) {
+  if (size === 0) return 0;
+  let solutions = 0;
+  let board = new Array(size).fill(0);
+  let diag = new Array(size*2-1).fill(0);
+  let antiDiag = diag.slice();
+  placeQueen(board, diag, antiDiag, 0);
 
+  function findDiagonal (x, y) {
+    let diagonal = -(x-y-(size-1)); //different ways to express based upon readability
+    let antiDiagonal = x+y;
+    return [diagonal, antiDiagonal];
+  }
+  // Make two arrays with (2*size)-1 elements, which update whenever we add or remove a queen
+  // We can update these by invoking findDiagonal
+
+  function placeQueen (row, diagonal, antiDiagonal, queens) { //Rooks can represent which row we're in
+    for (let column = 0; column < size; column++) {
+      let [diagIndex, antiIndex] = findDiagonal(column, queens);
+      
+      // Open one new recursive call for each available 0
+      if (row[column] === 0 && diagonal[diagIndex] === 0 && antiDiagonal[antiIndex] === 0) {
+        row[column] = 1;
+        diagonal[diagIndex] = 1;
+        antiDiagonal[antiIndex] = 1;
+        queens++;
+        //let [diagonal, antiDiagonal] = findDiagonal(i, queens);
+
+        // If we just placed our last rook, count it as a solution and stop
+        if (queens === row.length) {
+          solutions++;
+        } else {
+          placeQueen(row, diagonal, antiDiagonal, queens);
+        }
+        row[column] = 0;
+        diagonal[diagIndex] = 0;
+        antiDiagonal[antiIndex] = 0;
+        queens--;
+      }
+    }
+  }
+  return solutions;
 }
 
 module.exports = calc;
+
+
--- scripts/rooks.js
+++ scripts/rooks.js
@@ -1,7 +1,29 @@
 'use strict';
 
+
 function calc (size) {
+  let solutions = 0;
+  let board = new Array(size).fill(0);
+  placeRook(board, size);
 
+  function placeRook (row, rooks) { 
+    for (let column = 0; column < size; column++) {
+      // Open one new recursive call for each available 0
+      if (row[column] === 0) {
+        row[column] = 1;
+        rooks--;
+        // If we just placed our last rook, count it as a solution and stop
+        if (rooks === 0) {
+          solutions++;
+        } else {
+          placeRook(row, rooks);
+        }
+        row[column] = 0;
+        rooks++;
+      }
+    }
+  }
+  return solutions;
 }
-
 module.exports = calc;
+
